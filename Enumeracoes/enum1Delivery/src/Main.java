5189c96fa2bf6142329d38b87d40db0da44e5183
Alexandre Abreu Silva <contatoalexandre95@gmail.com>
2024-03-11 18:11:37 -0300

    adding enum learning java

---
+++ Enumeracoes/enum1Delivery/src/Main.java
@@ -0,0 +1,16 @@
+import entities.Order;
+import enums.OrderStatus;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        //Transformando em string o OrderStatus.
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus osEnviado = OrderStatus.valueOf("Enviado!");
+    }
+}
